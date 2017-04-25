d27d605a43e517d7df2b22e2314e2eb6ae818dbf
pellsson <pellelindblad@gmail.com>
2017-04-25 23:31:52 +0200

    hehjehe

--- gen_all_24.sh
+++ gen_all_24.sh
@@ -2,4 +2,4 @@ span=$((24*60)) # 24 hours
 duration=60 # 1 minute duration
 fps=20 # 20 fps
 out=24hrs.mp4
-find /dev/v4l/by-id/ -type l -printf "%f\n" | xargs -I{} sh gen_vid.sh {} $span $duration $fps $out
+find /dev/v4l/by-id/ -type l -printf "%f\n" | xargs -I{} sh gen_vid.sh images/{} $span $duration $fps $out
