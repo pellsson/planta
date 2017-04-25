ab8c8c10b71c42cca3bc47f0daed489b756c6960
Pellsson <pellelindblad@gmail.com>
2017-04-25 18:54:52 +0200

    Path

--- snap.sh
+++ snap.sh
@@ -1,3 +1,4 @@
+cd $(dirname $0)
 now=$(date +%Y-%m-%d_%H:%M)
 find /dev/v4l/by-id/ -type l -printf "%f\n" | xargs -I{} mkdir -p images/{}
 find /dev/v4l/by-id/ -type l -printf "%f\n" | xargs -I{} avconv -f video4linux2 -s 1280x960 -i /dev/v4l/by-id/{} -vframes 1 images/{}/$now.jpg
