3fd99f96a4e01d53a480fcc567827baf9ab011d7
Pellsson <pellelindblad@gmail.com>
2017-04-26 14:28:03 +0200

    Oops

--- gen_all_week.sh
+++ gen_all_week.sh
@@ -3,6 +3,6 @@ cd $(dirname $0)
 span=$((7*24*60)) # 7 days
 duration=60 # 1 minute duration
 fps=20 # 20 fps
-out=24hrs.mp4
+out=week.mp4
 find /dev/v4l/by-id/ -type l -printf "%f\n" | xargs -I{} sh gen_vid.sh images/{} $span $duration $fps $out
 
